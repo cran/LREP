659fc40e7da365f033ff4a415ba45df78eef2f29
Jiqiang Wu <charles.wu@utah.edu>
2021-07-29 07:50:14 +0000

    version 0.1.0

---
+++ R/LREP-package.R
@@ -0,0 +1,13 @@
+#' @references
+#' \enumerate{
+#' \item Kozubowski, T.J., A.K. Panorska, F. Qeadan, and A. Gershunov (2007). Testing exponentiality versus Pareto distribution via likelihood ratio, preprint in review.
+#' \item Panorska, A.K., A. Gershunov, and T.J. Kozubowski (2007). From diversity to volatility: Probability of daily precipitation and extremes. Nonlinear Dynamics in Geosciences (A. Tsonis and J. Elsner, Eds.), Springer, New York, pp 465-484.
+#' }
+#' @docType package
+#' @name LREP-package
+"_PACKAGE"
+
+NULL
+
+
+
